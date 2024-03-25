62f86cd39efdb66b07dd23a25c886a2d40bc6a74
Your Name <nguyphuquy1@gmail.com>
2024-03-25 10:15:24 +0700

    update2

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,35 @@
 import './home.scss'
 import { Carousel } from '@/components/carousels/Carousel'
 import { Product } from '@/components/product/Product'
+import { Modal } from 'antd';
+import img from '../../../public/img/technologies.png'
+import { useEffect } from 'react';
 export default function Home() {
+  useEffect(() => {
+    Modal.info({
+      title: "Welcome to Nguy Phu Quy's website!",
+      content: (
+        <div style={{ width: '700px', height: '600px' }}>
+          <p>Some information about my website.</p>
+          <img src={img} alt="Description of image" style={{ maxWidth: '100%', height: 'auto' }} />
+          <p>Facebook: facebook.com/phuquy119</p>
+          <p>Zalo: 0936549721</p>
+          <p>Github Client: https://github.com/mijsmijs1/PROJECT_MD05_CLIENT</p>
+          <p>Github Server: https://github.com/mijsmijs1/PROJECT_MD05_SERVER</p>
+        </div>
+      ),
+      width: '42%',
+      onOk: () => {
+        // Thêm xử lý khi nhấn nút "OK" ở đây nếu cần
+      }
+    });
+  }, [])
   return (
     <div className='home_app'>
-                <div className='home_container'>
-                    <Carousel/>
-                    <Product/>
-                </div>
-            </div>
+      <div className='home_container'>
+        <Carousel />
+        <Product />
+      </div>
+    </div>
   )
 }
--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -2,31 +2,12 @@ import Footer from '@/components/footer/Footer'
 import Navbar from '@/components/navbar/Navbar'
 import './layout.scss'
 import { Outlet } from 'react-router-dom'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Login from '@/components/login/Login';
-import { Modal } from 'antd';
-import img from '../../../public/img/technologies.png'
+
 export default function Layout() {
     const [modalVisible, setModalVisible] = useState(false);
-    useEffect(() => {
-        Modal.info({
-            title: "Welcome to Nguy Phu Quy's website!",
-            content: (
-                <div style={{ width: '700px', height: '600px' }}>
-                    <p>Some information about my website.</p>
-                    <img src={img} alt="Description of image" style={{ maxWidth: '100%', height: 'auto' }} />
-                    <p>Facebook: facebook.com/phuquy119</p>
-                    <p>Zalo: 0936549721</p>
-                    <p>Github Client: https://github.com/mijsmijs1/PROJECT_MD05_CLIENT</p>
-                    <p>Github Server: https://github.com/mijsmijs1/PROJECT_MD05_SERVER</p>
-                </div>
-            ),
-            width: '42%',
-            onOk: () => {
-                // Thêm xử lý khi nhấn nút "OK" ở đây nếu cần
-            }
-        });
-    }, [])
+
     return (
         <div className='layout_container'>
 
